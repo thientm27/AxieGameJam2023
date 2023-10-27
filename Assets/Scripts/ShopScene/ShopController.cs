34b6fa3b8d47964cd82e5790533203d4062853e8
thientm27 <denkhotieu1@gmail.com>
2023-10-28 01:37:24 +0700

    Select able shop

--- Assets/Scripts/ShopScene/ShopController.cs
+++ Assets/Scripts/ShopScene/ShopController.cs
@@ -1,4 +1,4 @@
-using System;
+using DG.DemiEditor;
 using Services;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,11 +10,11 @@ namespace ShopScene
         [SerializeField] private ShopView view;
         [SerializeField] private ShopModel model;
 
-        
+
         private DisplayShop _currentChoose;
         private int _index;
         private PlayerService _playerService;
-        
+
 
         private void Awake()
         {
@@ -29,47 +29,87 @@ namespace ShopScene
                 SceneManager.LoadScene(Constants.EntryScene);
                 return;
             }
-            
-            
+
+
             // VALUES
             _index = 0;
             _currentChoose = DisplayShop.Armor;
-            
+
             // INIT SHOP 1 VALUE
-            int[] tmp =  new int[4];
+            int[] tmp = new int[4];
             tmp[0] = model.armorPrices[_playerService.ArmoryLevel[0]];
             tmp[1] = model.weaponPrices[_playerService.ArmoryLevel[1]];
             tmp[2] = model.bootsPrices[_playerService.ArmoryLevel[2]];
             tmp[3] = model.rocketPrices[_playerService.ArmoryLevel[3]];
             view.contentShops[0].Initialized(_playerService.ArmoryLevel.ToArray(),
                 tmp);
-            
+
             // INIT SHOP 2 VALUE
-            tmp =  new int[4];
+            tmp = new int[4];
             tmp[0] = model.accessory0[_playerService.AccessoryLevel[0]];
             tmp[1] = model.accessory1[_playerService.AccessoryLevel[1]];
             tmp[2] = model.accessory2[_playerService.AccessoryLevel[2]];
             tmp[3] = model.accessory3[_playerService.AccessoryLevel[3]];
             view.contentShops[1].Initialized(_playerService.AccessoryLevel.ToArray(),
                 tmp);
-            
+
             // Display first shop
-            for (int i = 0; i < view.contentShops.Count; i++)
-            {
-                view.contentShops[i].gameObject.SetActive(i == 0);
-            }
-            
-            
+            view.ChangeTab(_currentChoose);
         }
 
         private void Update()
         {
-            
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                HandleSelector(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                HandleSelector(false);
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                SwitchTab(false);
+            }
+        }
+
+        private void SwitchTab(bool isRight)
+        {
+            _index = 0;
+            view.contentShops[0].SetIndicator(_index);
+            view.contentShops[1].SetIndicator(_index);
+            switch (_currentChoose)
+            {
+                case DisplayShop.Armor:
+                    _currentChoose = DisplayShop.Accessories;
+                    break;
+                case DisplayShop.Accessories:
+                    _currentChoose = DisplayShop.Armor;
+                    break;
+            }
+
+            view.ChangeTab(_currentChoose);
+        }
+
+        private void HandleSelector(bool isDown)
+        {
+            _index = isDown ? _index == 3 ? 0 : _index + 1 : _index == 0 ? 3 : _index - 1;
+            switch (_currentChoose)
+            {
+                case DisplayShop.Armor:
+                    view.contentShops[0].SetIndicator(_index);
+                    break;
+                case DisplayShop.Accessories:
+                    view.contentShops[1].SetIndicator(_index);
+                    break;
+            }
         }
 
         private void HandleBuy()
         {
-            
         }
     }
 
--- Assets/Scripts/ShopScene/ShopView.cs
+++ Assets/Scripts/ShopScene/ShopView.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using ShopScene.Component;
+using TMPro;
 using UnityEngine;
 
 namespace ShopScene
@@ -7,5 +9,31 @@ namespace ShopScene
     public class ShopView : MonoBehaviour
     {
         public List<ContentShop> contentShops;
+        public List<TextMeshProUGUI> textContentShops;
+
+        public void ChangeTab(DisplayShop newTab)
+        {
+            switch (newTab)
+            {
+                case DisplayShop.Armor:
+                    contentShops[0].gameObject.SetActive(true);
+                    textContentShops[0].color = Color.yellow;
+                    textContentShops[0].gameObject.transform.DOScale(new Vector2(1.2f, 1.2f),0.5f);
+                    
+                    contentShops[1].gameObject.SetActive(false);
+                    textContentShops[1].color = Color.white;
+                    textContentShops[1].gameObject.transform.DOScale(Vector3.one, 0.5f);
+                    break;
+                case DisplayShop.Accessories:
+                    contentShops[0].gameObject.SetActive(false);
+                    textContentShops[0].color = Color.white;
+                    textContentShops[0].gameObject.transform.DOScale(Vector3.one, 0.5f);
+
+                    contentShops[1].gameObject.SetActive(true);
+                    textContentShops[1].color = Color.yellow;
+                    textContentShops[1].gameObject.transform.DOScale(new Vector2(1.2f, 1.2f),0.5f);
+                    break;
+            }
+        }
     }
 }
