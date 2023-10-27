fc61640dd8ab4acf3c917cf32c3a3655eb634635
NGOCKHA\ngock <khanguyen.jobs@gmail.com>
2023-10-27 20:29:03 +0700

    d

--- Assets/Scripts/Game/Monster.cs
+++ Assets/Scripts/Game/Monster.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    public int HP { get; set; }
+    public int AttackRate { get; set; }
     public void GotHit()
+    {
+
+    }
+    public void Attack()
     {
 
     }
